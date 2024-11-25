f7049ed7f5cfd3e1a1b27a94d4dddf29faea8bcb
liuyangyang <www.qq37374741@foxmail.com>
2024-11-25 21:23:12 +0800

    Fixed the issue that there was no data on the serial port

--- sdk/infinite_sense_core/x86/include/serial_manager.h
+++ sdk/infinite_sense_core/x86/include/serial_manager.h
@@ -8,7 +8,7 @@
 #include "crc.h"
 
 class SerialManager {
-  static constexpr size_t IMU_DATA_NUM = 56;
+  static constexpr size_t IMU_DATA_NUM = 52;
   static constexpr size_t BUF_LEN = 65540;
 
  public:
